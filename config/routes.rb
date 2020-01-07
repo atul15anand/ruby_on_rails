f239e1dd4c338943af4923bd1f9153e604c505b1
alma <atul15anand@gmail.com>
2020-01-07 15:12:08 +0530

    Version-1.0.0

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+#get method
+root 'beta#index'
+#get 'welcome/index', to: 'welcome#index'
+#get 'welcome/home', to: 'welcome#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
